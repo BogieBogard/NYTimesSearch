7ffed014a277b9990635bbbac731372fade8c666
BogieBogard <mbogard@utexas.edu>
2018-07-07 13:13:50 -0500

    Small corrections

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,15 @@
 var apiKey =  "855c119719604ba88c9af03e9c6e75f5";
 var searchTerms = "trump" // change to grab user input
-var beginDate = "2016" // more user input
-var endDate = "2018" //woooooooo
+// Dates need to be in the format of YEARMMDD
+var beginDate = "20160101" // more user input
+var endDate = "20180101" //woooooooo
 
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
   'api-key': `${apiKey}`,
   'q': `${searchTerms}`,
-  'begin_date': `${beignDate}`,
+  'begin_date': `${beginDate}`,
   'end_date': `${endDate}`,
   'page': '0'
 });
